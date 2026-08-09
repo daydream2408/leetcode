950d39b90292c84c116ef9ea7cea9d06e8e744e4
daydream2408 <daydream26jan2020@gmail.com>
2026-08-09 23:17:01 +0530

    Time: 2 ms (37%), Space: 69.5 MB (66.23%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -5,18 +5,21 @@ public:
         int r=0;
         int zero=0;
         int maxlen=0;
+        int store;
         while(r<nums.size()){
              if(nums[r]==0){
                 zero++;
+               
              }
-             
-            while(zero>k){
+
+            if(zero>k){
                if(nums[l]==0) zero--;
                l++;
+               
             }
             if(zero<=k){
              maxlen=max(maxlen,r-l+1);
-            
+               //if(nums[r]==0)  store=r;
             }
             r++;
         }

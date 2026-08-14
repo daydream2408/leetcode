1085d68ade79909114ed5e471635f4662ff0e250
daydream2408 <daydream26jan2020@gmail.com>
2026-08-14 14:07:16 +0530

    Time: 42 ms (12.85%), Space: 11.6 MB (43.43%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,54 +1,53 @@
 class Solution {
 public:
-    string minWindow(string s, string t) {
-        if (s.size() < t.size()) return "";
-
-        int hash[256] = {0};
-
-        for (char c : t) {
-            hash[c]++;
+    bool pos(vector<int>&curr_small,vector<int>&curr_big,vector<int>&req_small,vector<int>&req_big){
+        for(int i=0;i<26;i++){
+            if(req_small[i]>curr_small[i]) return false;
+            if(req_big[i]>curr_big[i]) return false;
         }
-
-        int l = 0;
-        int r = 0;
-        int cnt = 0;
-
-        int minlen = INT_MAX;
-        int sindex = -1;
-
-        while (r < s.size()) {
-
-            // Add s[r]
-            if (hash[s[r]] > 0) {
-                cnt++;
-            }
-
-            hash[s[r]]--;
-
-            // Try shrinking the window
-            while (cnt == t.size()) {
-
-                if (r - l + 1 < minlen) {
-                    minlen = r - l + 1;
-                    sindex = l;
+        return true;
+    }
+    string minWindow(string s, string t) {
+        vector<int> req_small(26, 0);
+        vector<int> req_big(26, 0);
+        for (char ch : t) {
+            if (ch >= 'a' && ch <= 'z')
+                req_small[ch - 'a']++;
+            else
+                req_big[ch - 'A']++;
+        }
+        vector<int> curr_small(26, 0);
+        vector<int> curr_big(26, 0);
+        int s_i = -1;
+        int e_i = -1;
+        int prev_len = INT_MAX;
+        int n = s.size();
+        int l = 0, r = 0;
+        while (r < n) {
+            if (s[r] >= 'a' && s[r] <= 'z')
+                curr_small[s[r] - 'a']++;
+            else
+                curr_big[s[r] - 'A']++;
+            while (l <= r && pos(curr_small, curr_big, req_small, req_big)) {
+                int len = r - l + 1;
+                if (len < prev_len) {
+                    s_i = l;
+                    e_i = r;
+                    prev_len = len;
                 }
-
-                // Remove s[l]
-                hash[s[l]]++;
-
-                if (hash[s[l]] > 0) {
-                    cnt--;
-                }
-
+                if (s[l] >= 'a' && s[l] <= 'z')
+                    curr_small[s[l] - 'a']--;
+                else
+                    curr_big[s[l] - 'A']--;
                 l++;
             }
-
             r++;
         }
-
-        if (sindex == -1)
+        if (s_i == -1)
             return "";
-
-        return s.substr(sindex, minlen);
+        string res;
+        for (int i = s_i; i <= e_i; i++)
+            res.push_back(s[i]);
+        return res;
     }
 };

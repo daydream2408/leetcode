10a68de071aa39d23c6d9bdb04a4a4a34b17f469
daydream2408 <daydream26jan2020@gmail.com>
2026-09-14 20:36:47 +0530

    Time: 16 ms (5.97%), Space: 69.7 MB (5.17%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -3,26 +3,20 @@ public:
     int longestOnes(vector<int>& nums, int k) {
         int l=0;
         int r=0;
-        int zero=0;
-        int maxlen=0;
-        int store;
-        while(r<nums.size()){
-             if(nums[r]==0){
-                zero++;
-               
-             }
+        int count=0;
+        int len=0;
+      unordered_map<int,int>mp;
 
-            if(zero>k){
-               if(nums[l]==0) zero--;
-               l++;
-               
+        while(r<nums.size()){
+            mp[nums[r]]++;
+           
+            if(mp[0]>k){
+                mp[nums[l]]--;
+                l++;
             }
-            if(zero<=k){
-             maxlen=max(maxlen,r-l+1);
-               //if(nums[r]==0)  store=r;
-            }
+            len=max(len,r-l+1);
             r++;
         }
-        return maxlen;
+        return len;
     }
 };

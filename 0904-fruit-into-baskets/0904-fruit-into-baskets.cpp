9176fb3c2bd385f3f5ad8a2a02d04f1a6153686d
daydream2408 <daydream26jan2020@gmail.com>
2026-09-14 20:53:55 +0530

    Time: 35 ms (81.92%), Space: 84.9 MB (34.62%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -3,21 +3,21 @@ public:
     int totalFruit(vector<int>& fruits) {
         int l=0;
         int r=0;
-        int n=fruits.size();
-        int maxlen=0;
-        unordered_map<int,int>hash;
-        while(r<n){
-            hash[fruits[r]]++;
+        int len=0;
+        unordered_map<int,int>mp;
 
-            while(hash.size()>2){
-                hash[fruits[l]]--;
-                if(hash[fruits[l]]==0) hash.erase(fruits[l]);
-                l++;
+        while(r<fruits.size()){
+            mp[fruits[r]]++;
 
+            while(mp.size()>2){
+                 mp[fruits[l]]--;
+                 if(mp[fruits[l]]==0) mp.erase(fruits[l]);
+                 l++;
             }
-            maxlen= max(maxlen,r-l+1);
+
+            len=max(len,r-l+1);
             r++;
         }
-        return maxlen;
+        return len;
     }
 };

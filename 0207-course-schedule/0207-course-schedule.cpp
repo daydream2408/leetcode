2671e4121de32dfdb21b7043870cffd83a9a4421
daydream2408 <daydream26jan2020@gmail.com>
2026-09-26 13:48:31 +0530

    Time: 3 ms (76.5%), Space: 20.1 MB (25.62%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,37 +1,42 @@
 class Solution {
 public:
-  bool dfs(int node, vector<int>&vis, vector<int>&pathvis, vector<vector<int>>&adj){
-    vis[node]=1;
-    pathvis[node]=1;
-    
-    for(auto it : adj[node]){
-        if(!vis[it]){
-            if(dfs(it,vis,pathvis,adj)==true) return true;
-        }
-        else if(pathvis[it]) return true;
-    }
+ 
 
-
-
-    pathvis[node]=0;
-    return false;
-}
 
     bool canFinish(int n, vector<vector<int>>& p) {
-          vector<vector<int>>adj(n);
+        //if(p.empty())return true;
+        vector<int>indegree(n,0);
+         
+        vector<vector<int>>sexy(n);
 
-       for(auto it:p ){
-        adj[it[1]].push_back(it[0]);
-       }
+         for(auto it: p){
+            sexy[it[1]].push_back(it[0]);
+            indegree[it[0]]++;
+         }
+         
 
-       vector<int>vis(n);
-       vector<int>pathvis(n);
+         queue<int>q;
 
-       for(int i=0; i<n ;i++){
-        if(!vis[i]){
-            if(dfs(i,vis,pathvis,adj)==true) return false;
-        }
-       }
-       return true;
+         for(int i=0; i<n;i++){
+            if(indegree[i]==0) q.push(i);
+         }
+
+          while(!q.empty()){
+            int curr=q.front();
+            q.pop();
+
+            for(auto neighbour:sexy[curr]){
+
+                  indegree[neighbour]--;
+                  if(indegree[neighbour]==0) q.push(neighbour);
+                
+            }
+
+          }
+            for(auto it: indegree){
+                if(it!=0) return false;
+            }
+
+        return true;
     }
 };
